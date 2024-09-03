6d8168c50989e6680ecf5aec1a1ce8ddbeb80a94
Nico <npapelard@gmail.com>
2024-09-03 16:47:36 +0200

    CommandeRemplacer implémenté
    CommandeRemplacer correction d'un bug
    
    Intégration de la commande remplacer dans CommandeFactory

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);}
+
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String contenu ="";
+        contenu+=parameters[3];
+
+        this.document.remplacer(debut,fin,contenu);
+        super.executer();
+    }
+}
